531fe1b9d25e773fcb1eb0a8ace4d7c86428e8cf
Stratos Thivaios <me@sthivaios.dev>
2026-05-25 02:22:53 +0300

    gitignore certs dir

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Stratos Thivaios <me@sthivaios.dev>
+ *
+ * This file is part of the AridLink 1 Firmware.
+ *
+ * AridLink 1 Firmware is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AridLink 1 Firmware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AridLink 1 Firmware. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
 void app_main(void)
