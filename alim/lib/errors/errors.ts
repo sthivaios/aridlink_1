3c181f32d691abb3a4c73ca01e65c7d9353007e6
Stratos Thivaios <me@sthivaios.dev>
2026-06-30 02:31:42 +0300

    add `internal_server_error_response` for standardized 500 error handling

--- alim/lib/errors/errors.ts
+++ alim/lib/errors/errors.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export class AlimError extends Error {
   code: string;
   http_code: number;
@@ -9,3 +11,12 @@ export class AlimError extends Error {
     this.http_code = http_code;
   }
 }
+
+export const internal_server_error_response = NextResponse.json(
+  {
+    error_code: "ALE-99-9999",
+    error_message:
+      "Internal unhandled server exception. You should report this, and describe what you were doing at aridlink@sthivaios.dev.",
+  },
+  { status: 500 }
+);
