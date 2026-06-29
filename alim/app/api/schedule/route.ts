ade144bcb09560b54653af1df13d6283f3fb0535
Stratos Thivaios <me@sthivaios.dev>
2026-06-30 02:32:35 +0300

    refactor `GET` schedule route with standardized error handling

--- alim/app/api/schedule/route.ts
+++ alim/app/api/schedule/route.ts
@@ -1,32 +1,38 @@
-import { NextResponse } from "next/server"
-import { tryCatch } from "@/lib/try-catch"
-import prisma from "@/lib/prismacilent"
+import { NextResponse } from "next/server";
+import { authenticateDevice } from "@/lib/authenticate-device";
+import { tryCatch } from "@/lib/try-catch";
+import { AlimError, internal_server_error_response } from "@/lib/errors/errors";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const imei = searchParams.get("imei")
+  const { data, error } = await tryCatch(authenticateDevice(request));
 
-  if (!imei) {
-    return NextResponse.json({ error: "Missing IMEI parameter! ALIM needs to know which schedule to fetch." }, { status: 400 })
+  // check if the device authentication errored
+  if (error) {
+    // check if the error is an ALIM error class
+    if (error instanceof AlimError) {
+      // return it if it is
+      return NextResponse.json({
+        error_code: error.code,
+        error_message: error.message
+      }, { status: error.http_code })
+    } else {
+      // return the internal unhandled error response if its not an alim error
+      return internal_server_error_response;
+    }
   }
 
-  const {data, error} = await tryCatch(
-    prisma.device.findUnique({
-      where: {
-        imei: imei,
-      },
-    })
-  );
-
-  if (error || data == null) {
-    return NextResponse.json(
-      {
-        error:
-          "The device does not exist on the ALIM database, or the IMEI is incorrect.",
-      },
-      { status: 404 }
-    )
+  if (!data) {
+    // return the internal unhandled error response if the data is somehow null
+    return internal_server_error_response;
   }
 
-  return NextResponse.json(data.schedule);
+  // return the schedule and info about it
+  return NextResponse.json({
+    device: {
+      device_imei: data.imei,
+      device_name: data.name,
+      schedule_updated: data.scheduleLastUpdated
+    },
+    schedule: data.schedule
+  });
 }
