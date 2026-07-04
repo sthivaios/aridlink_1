ca51e983a277ad1d6cf4b7cd4923a8e449c61970
Stratos Thivaios <me@sthivaios.dev>
2026-07-04 03:49:23 +0300

    feat(scripts): add dev-keygen.sh for generating development device keys

---
+++ alim/dev-keygen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "ALIM Development Device Key Generator"
+echo "==========================================="
+read -p "KEY LENGTH (INT) > " length
+
+bytes=$((length / 2))
+
+key=$(openssl rand -hex $bytes)
+hashed=$(echo $key | sha256)
+
+echo KEY: $key
+echo SHA256: $hashed
