db7f4e45f08edc940da9406dea03ddb1829a6e3f
Stratos Thivaios <me@sthivaios.dev>
2026-07-06 01:40:03 +0300

    remove `lastSeen` field from `Device` model

---
+++ alim/prisma/migrations/20260705223944_remove_last_seen_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastSeen` on the `Device` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Device" DROP COLUMN "lastSeen";
