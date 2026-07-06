0c46b9dbd75e35cc52955ae96fd28a9f8dae7a31
Stratos Thivaios <me@sthivaios.dev>
2026-07-06 04:18:10 +0300

    add `get_sha256` utility function for SHA-256 hashing

--- alim/lib/utils.ts
+++ alim/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { createHash } from "node:crypto";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function get_sha256(input: string): string {
+  return createHash("sha256").update(input).digest("hex");
 }
