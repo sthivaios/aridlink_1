ad6b4b2c644be7471fd3286abc3744b86d813682
Stratos Thivaios <me@sthivaios.dev>
2026-07-13 11:01:04 +0300

    rename OpenDeviceBadge to OpenEntityBadge and add support for custom titles

--- alim/components/open-entity-badge.tsx
+++ alim/components/open-entity-badge.tsx
@@ -1,12 +1,12 @@
 import { Badge } from "@/components/ui/badge";
 import { ArrowUpRightIcon } from "lucide-react";
 
-export function OpenDeviceBadge(props: { href: string }) {
+export function OpenEntityBadge(props: { href: string, title: string }) {
   return (
     <div className="flex flex-wrap gap-2">
       <Badge asChild variant="default" className="">
         <a href={props.href} className="flex flex-row items-center">
-          <span className="leading-none">View device</span> <ArrowUpRightIcon data-icon="inline-end" />
+          <span className="leading-none">{props.title}</span> <ArrowUpRightIcon data-icon="inline-end" />
         </a>
       </Badge>
     </div>
