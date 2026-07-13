8ca35e56c8a299eba12841c392bb1a8d70367a29
Stratos Thivaios <me@sthivaios.dev>
2026-07-13 11:12:01 +0300

    add schedule edit page to view and update existing CSPs

---
+++ alim/app/dashboard/schedules/[slug]/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { UpsertScheduleForm } from "@/app/dashboard/schedules/upsertScheduleForm";
+import prisma from "@/lib/prismacilent";
+import { tryCatch } from "@/lib/try-catch";
+import ErrorCard from "@/components/error-card";
+import { parseValves } from "@/lib/parse-valves-json";
+
+async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+
+  const { data, error } = await tryCatch(
+    prisma.scheduleProfile.findUnique({
+      where: {
+        id: slug
+      },
+      include: {
+        devices: {}
+      }
+    })
+  );
+
+  if (error || (data == null)) {
+    return (
+      <ErrorCard title="Schedule not found">
+        <p>The requested CSP could not be found.</p>
+      </ErrorCard>
+    )
+  }
+
+  return (
+    <div>
+      <UpsertScheduleForm valves={parseValves(data.schedule)} fullCspObject={data} />
+    </div>
+  );
+}
+
+export default Page;
