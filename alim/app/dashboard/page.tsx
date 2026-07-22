5d0e0eb61f4d00cf2df3fc98ca311e90a8931788
Stratos Thivaios <me@sthivaios.dev>
2026-07-22 22:43:53 +0300

    simplify dashboard landing page

--- alim/app/dashboard/page.tsx
+++ alim/app/dashboard/page.tsx
@@ -1,66 +1,14 @@
 import React from "react";
-import { tryCatch } from "@/lib/try-catch";
-import prisma from "@/lib/prismacilent";
-import ErrorCard from "@/components/error-card";
-import { DeviceTable } from "@/components/device-table";
-import { Panel } from "@/components/primitives";
-import { Info } from "lucide-react";
 
 async function Page() {
-  const { data, error } = await tryCatch(
-    prisma.device.findMany({
-      include: {
-        heartbeats: {
-          orderBy: { createdAt: "desc" },
-          take: 1,
-        },
-      },
-    })
-  );
-
-  if (error) {
-    return (
-      <ErrorCard title="Error fetching devices">
-        <p className="wrap-anywhere">
-          There was an error while attempt to fetch the devices from the
-          database. Details: {error?.message}
-        </p>
-      </ErrorCard>
-    );
-  }
-
   return (
-    <div className="flex w-full flex-col items-center gap-5">
-      {data.filter((d) => d.approved).length > 0 ? (
-        <Panel className="flex w-full flex-col items-center justify-center gap-10 p-10">
-          <p className="relative px-20 pb-2 font-bold after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-linear-to-r after:from-transparent after:via-muted-foreground/50 after:to-transparent">
-            Devices registered to this server
-          </p>
-          <DeviceTable devices={data.filter((d) => d.approved)} />
-        </Panel>
-      ) : (
-        <div className="flex flex-row items-center gap-2">
-          <Info className="h-4 w-4 stroke-muted-foreground" />
-          <p className="text-muted-foreground italic">
-            There are no devices registered on this server
-          </p>
-        </div>
-      )}
-      {data.filter((d) => !d.approved).length > 0 ? (
-        <Panel className="flex w-full flex-col items-center justify-center gap-10 p-10">
-          <p className="relative px-20 pb-2 font-bold after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-linear-to-r after:from-transparent after:via-muted-foreground/50 after:to-transparent">
-            Devices pending approval to be registered on this server
-          </p>
-          <DeviceTable devices={data.filter((d) => !d.approved)} />
-        </Panel>
-      ) : (
-        <div className="flex flex-row items-center gap-2">
-          <Info className="h-4 w-4 stroke-muted-foreground" />
-          <p className="text-muted-foreground italic">
-            There are no devices pending approval into ALIM
-          </p>
-        </div>
-      )}
+    <div className="flex h-[80vh] w-full flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">ALIM Dashboard</h1>
+      <p className="text-center text-lg">
+        This dashboard provides access to ALIM system functions and reporting.
+        <br />
+        Use the navigation panel on the left to perform an action.
+      </p>
     </div>
   );
 }
