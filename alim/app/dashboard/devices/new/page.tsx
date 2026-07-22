e8f5a51335757d9669b8077225cd58ab43149cad
Stratos Thivaios <me@sthivaios.dev>
2026-07-22 22:45:52 +0300

    add new-device page

---
+++ alim/app/dashboard/devices/new/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NewDeviceForm from "@/app/dashboard/devices/new/newDeviceForm";
+import { tryCatch } from "@/lib/try-catch";
+import prisma from "@/lib/prismacilent";
+
+async function Page() {
+  const { data: CSPs, error: ErrorFetchingCSPs } = await tryCatch(
+    prisma.scheduleProfile.findMany({})
+  );
+
+  return (
+    <div>
+      <NewDeviceForm CSPs={CSPs} CSPs_Error={!!ErrorFetchingCSPs} />
+    </div>
+  );
+}
+
+export default Page;
