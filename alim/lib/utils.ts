5bbab851d49a5271de57c9feffc17e599a8a18b0
Stratos Thivaios <me@sthivaios.dev>
2026-07-22 20:20:12 +0300

    add generate_device_key() function to utils.ts

--- alim/lib/utils.ts
+++ alim/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { createHash } from "node:crypto";
+import { createHash, randomBytes } from "node:crypto";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,4 +8,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function get_sha256(input: string): string {
   return createHash("sha256").update(input).digest("hex");
+}
+
+export function generate_device_key(): string {
+  return randomBytes(64).toString("hex");
 }
