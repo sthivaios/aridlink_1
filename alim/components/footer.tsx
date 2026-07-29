4482b0512cecfb5ae66e26a4c6f5c3e9a9a3028b
Stratos Thivaios <me@sthivaios.dev>
2026-07-29 03:32:37 +0300

    update footer links and add "About" page reference

--- alim/components/footer.tsx
+++ alim/components/footer.tsx
@@ -4,19 +4,37 @@ import Link from "next/link";
 function Footer(props: { alignment: "left" | "right" }) {
   return (
     <div className="flex w-max flex-col items-start justify-center gap-4 text-[13px] text-foreground">
-      <div className={`flex flex-col justify-center gap-0 ${props.alignment == "left" ? "items-start" : "items-end"}`}>
+      <div
+        className={`flex flex-col justify-center gap-0 ${props.alignment == "left" ? "items-start" : "items-end"}`}
+      >
         <p className="mb-2">AridLink Irrigation Manager v0.9.2</p>
         <p className="text-left font-bold">Copyright © 2026 Stratos Thivaios</p>
         <p>
-          ALIM is free software under the{" "}
+          View the application source code{" "}
           <Link
             className="font-bold text-blue-400 transition-all duration-200 hover:text-primary hover:underline"
-            href="https://www.gnu.org/licenses/agpl-3.0.html"
+            href="https://github.com/sthivaios/aridlink_1"
             target="_blank"
           >
-            GNU Affero General Public License v3
+            on GitHub
           </Link>
         </p>
+        {/*<p>*/}
+        {/*  ALIM is free software under the{" "}*/}
+        {/*  <Link*/}
+        {/*    className="font-bold text-blue-400 transition-all duration-200 hover:text-primary hover:underline"*/}
+        {/*    href="https://www.gnu.org/licenses/agpl-3.0.html"*/}
+        {/*    target="_blank"*/}
+        {/*  >*/}
+        {/*    GNU Affero General Public License v3*/}
+        {/*  </Link>*/}
+        {/*</p>*/}
+        <Link
+          className="font-bold text-blue-400 transition-all duration-200 hover:text-primary hover:underline"
+          href="/about"
+        >
+          About AridLink Irrigation Manager
+        </Link>
       </div>
     </div>
   );
