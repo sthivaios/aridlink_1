87264ab8e8a7bea13bfaa7dab8867d49f29b4232
Stratos Thivaios <me@sthivaios.dev>
2026-08-18 02:10:13 +0300

    make CardPage component more flexible by adding className and noBackground props

--- alim/components/card-page.tsx
+++ alim/components/card-page.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { Card } from "@/components/ui/card";
 import Footer from "@/components/footer";
 
-function CardPage({ children }: { children: React.ReactNode }) {
+function CardPage({
+  className,
+  children,
+  noBackground,
+}: {
+  className: string;
+  children: React.ReactNode;
+  noBackground?: boolean;
+}) {
   return (
-    <div className="flex h-screen w-full flex-row items-center justify-center text-center ">
-      <div className="topographic-paper fixed inset-0 -z-10" />
-      <Card className="flex w-min flex-col items-center px-20 py-10">
+    <div className="flex h-screen w-full flex-row items-center justify-center text-center">
+      <div
+        className={`${noBackground ? "" : "topographic-paper"} fixed inset-0 -z-10`}
+      />
+      <Card className={`flex flex-col items-center px-20 py-10 ${className}`}>
         {children}
       </Card>
       <div className="fixed bottom-10 left-10 z-50">
