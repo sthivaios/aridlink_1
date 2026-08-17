5d3e45e7b9ae276df3b6795ac225c91393bb2615
Stratos Thivaios <me@sthivaios.dev>
2026-08-18 02:09:16 +0300

    temporarily hardcode trustedOrigins in the auth setup cuz im lazy lol

--- alim/lib/auth.ts
+++ alim/lib/auth.ts
@@ -5,4 +5,14 @@ import prisma from "@/lib/prismacilent";
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: { enabled: true, autoSignIn: false },
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://alim_development_server.twinknet.uk",
+    /* yes i actually do own this domain it was an inside joke,
+     * and yes this is an actual fqdn that im using for testing the firmware,
+     * i cant be arsed to setup environment variables right now and do this properly,
+     * so whoever is reading this in the commit history will have to cope lmao,
+     * yes im hardcoding it for now
+     */
+  ],
 });
