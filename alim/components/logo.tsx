33fff44ec6e5528cc6fb9fac14c6daf80586381c
Stratos Thivaios <me@sthivaios.dev>
2026-08-18 02:12:48 +0300

    refactor Logo component to support customizable text colors and improve alignment

--- alim/components/logo.tsx
+++ alim/components/logo.tsx
@@ -2,20 +2,22 @@ import Image from "next/image";
 
 import React from "react";
 
-function Logo() {
+function Logo(props: { textColors?: { title: string; caption: string } }) {
   return (
-    <div className="flex flex-row items-center gap-2.5">
+    <div className="flex flex-row items-center justify-start gap-3">
       <Image
         src="/aridlink_logo.png"
         alt="The AridLink Logo"
         width={50}
         height={50}
       ></Image>
-      <div className="leading-tight">
-        <div className=" text-xl font-bold tracking-tight">ALIM</div>
-        <div className=" text-[13px] tracking-[0.14em] text-muted-foreground uppercase">
+      <div className="flex flex-col justify-start text-left">
+        <p className={`text-xl font-black ${props.textColors?.title}`}>ALIM</p>
+        <p
+          className={`text-sm ${props.textColors?.caption ?? "text-muted-foreground"} font-bold`}
+        >
           AridLink Irrigation Manager
-        </div>
+        </p>
       </div>
     </div>
   );
