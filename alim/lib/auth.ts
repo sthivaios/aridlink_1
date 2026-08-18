24a624982b4eab9c6744b3545722e45852e556fc
Stratos Thivaios <me@sthivaios.dev>
2026-08-19 01:55:19 +0300

    fix misconfigured dev URLs in auth.ts and next.config.ts

--- alim/lib/auth.ts
+++ alim/lib/auth.ts
@@ -8,6 +8,7 @@ export const auth = betterAuth({
   trustedOrigins: [
     "http://localhost:3000",
     "https://alim_development_server.twinknet.uk",
+    "https://alim-development-server.twinknet.uk",
     /* yes i actually do own this domain it was an inside joke,
      * and yes this is an actual fqdn that im using for testing the firmware,
      * i cant be arsed to setup environment variables right now and do this properly,
--- alim/next.config.ts
+++ alim/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["alim_development_server.twinknet.uk"],
+  allowedDevOrigins: [
+    "alim_development_server.twinknet.uk",
+    "alim-development-server.twinknet.uk",
+  ],
   /* yes im also hardcoding this, deal with it, read the comment in lib/auth.ts lmao */
 };
 
